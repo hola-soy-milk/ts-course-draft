4c293177459b36be943a448483445a371c8dcaea
Ramón Huidobro <hola@ramonh.dev>
2022-01-16 10:58:52 +0100

    Undo lessons

--- 9-constructor/src/types/Dice.ts
+++ 9-constructor/src/types/Dice.ts
@@ -1,12 +1,6 @@
 export default class DieWrapper {
-  sides: number;
-
-  constructor(sides: number) {
-    this.sides = sides;
-  }
-
   value(): number {
-    let value: number = Math.floor(Math.random() * this.sides) + 1;
+    let value: number = Math.floor(Math.random() * 6) + 1;
     return value;
   }
 }
