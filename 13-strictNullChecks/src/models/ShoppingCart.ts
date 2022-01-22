41a667fdb363e05d15f282ba7a43d793223adca1
Ramón Huidobro <hola@ramonh.dev>
2022-01-22 15:00:12 +0100

    Add step 13

--- 13-strictNullChecks/src/models/ShoppingCart.ts
+++ 13-strictNullChecks/src/models/ShoppingCart.ts
@@ -13,31 +13,23 @@ export default class ShoppingCart {
     }
 
     groupedItems() {
-        if (this.items) {
-            return Object.values(this.items.reduce((cartItem, item) => {
-                cartItem[item.name()] = cartItem[item.name()] || {
-                    name: item.name(),
-                    quantity: 0,
-                    priceCents: item.priceCents()
-                };
-                cartItem[item.name()].quantity += 1;
-                cartItem[item.name()].priceCents += item.priceCents();
-                return cartItem;
-            }, {}));
-        } else {
-            return [];
-        }
+        return Object.values(this.items.reduce((cartItem, item) => {
+            cartItem[item.name()] = cartItem[item.name()] || {
+                name: item.name(),
+                quantity: 0,
+                priceCents: item.priceCents()
+            };
+            cartItem[item.name()].quantity += 1;
+            cartItem[item.name()].priceCents += item.priceCents();
+            return cartItem;
+        }, {}));
     }
 
     numberOfItems() {
-        return this.items ? this.items.length : 0;
+        return this.items.length;
     }
 
     total() {
-        if (this.items) {
-            return this.items.reduce((x, y) => x + y.priceCents(), 0);
-        } else {
-            return 0;
-        }
+        return this.items.reduce((x, y) => x + y.priceCents(), 0);
     }
 }
