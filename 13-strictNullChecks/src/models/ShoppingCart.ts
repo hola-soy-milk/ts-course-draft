72262fe75a4411d01fe93a2c865526c5eb770ebb
Ramón Huidobro <hola@ramonh.dev>
2022-01-23 16:15:34 +0100

    Chapter 13

--- 13-strictNullChecks/src/models/ShoppingCart.ts
+++ 13-strictNullChecks/src/models/ShoppingCart.ts
@@ -13,16 +13,20 @@ export default class ShoppingCart {
     }
 
     groupedItems() {
-        return Object.values(this.items.reduce((cartItem, item) => {
-            cartItem[item.name()] = cartItem[item.name()] || {
-                name: item.name(),
-                quantity: 0,
-                priceCents: item.priceCents()
-            };
-            cartItem[item.name()].quantity += 1;
-            cartItem[item.name()].priceCents += item.priceCents();
-            return cartItem;
-        }, {}));
+        if (this.items) {
+            return Object.values(this.items.reduce((cartItem, item) => {
+                cartItem[item.name()] = cartItem[item.name()] || {
+                    name: item.name(),
+                    quantity: 0,
+                    priceCents: item.priceCents()
+                };
+                cartItem[item.name()].quantity += 1;
+                cartItem[item.name()].priceCents += item.priceCents();
+                return cartItem;
+            }, {}));
+        } else {
+            return [];
+        }
     }
 
     numberOfItems() {
