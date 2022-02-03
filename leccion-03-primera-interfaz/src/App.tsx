b2221b9e85b4b190a8f0965490cb69b479c7743b
Ramón Huidobro <hola@ramonh.dev>
2022-02-03 17:09:30 +0100

    Rename folder

--- leccion-03-primera-interfaz/src/App.tsx
+++ leccion-03-primera-interfaz/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
-import Post from './models/Post';
+import Post from './types/Post';
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
