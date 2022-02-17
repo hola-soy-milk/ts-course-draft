e1f8032e9dd09d2ce4c783ca1a25493413907cc3
Ramón Huidobro <hola@ramonh.dev>
2022-02-17 21:58:26 +0100

    Rename stuff, style

--- leccion-12-ts-estricto-strictNullChecks/src/App.svelte
+++ leccion-12-ts-estricto-strictNullChecks/src/App.svelte
@@ -18,26 +18,23 @@
   
   <style>
 	:root {
-	  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+		Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 	}
-  
 	main {
 	  text-align: center;
 	  padding: 1em;
 	  margin: 0 auto;
 	}
-  
 	h1 {
-	  color: #ff3e00;
+	  color: var(--primary-color);
 	  text-transform: uppercase;
 	  font-size: 4rem;
 	  font-weight: 100;
 	  line-height: 1.1;
-	  margin: 2rem auto;
+	  margin: 2rem 0;
 	  max-width: 14rem;
 	}
-  
 	@media (min-width: 480px) {
 	  h1 {
 		max-width: none;
--- leccion-13-ts-estricto-noAny/src/components/Menu.svelte
+++ leccion-13-ts-estricto-noAny/src/components/Menu.svelte
@@ -10,12 +10,21 @@
 {/each}
 
 <style>
+  .flex {
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 75%;
+    justify-content: space-between;
+    justify-items: center;
+    gap: 6px;
+  }
   button {
     font-family: inherit;
     font-size: inherit;
     padding: 1em 2em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
+    color: var(--background-color);
+    background-color: var(--primary-color);
     border-radius: 2em;
     border: 2px solid rgba(255, 62, 0, 0);
     outline: none;
@@ -23,11 +32,9 @@
     font-variant-numeric: tabular-nums;
     cursor: pointer;
   }
-
   button:focus {
     border: 2px solid #ff3e00;
   }
-
   button:active {
     background-color: rgba(255, 62, 0, 0.2);
   }
